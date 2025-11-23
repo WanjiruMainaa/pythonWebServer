1fc83da7cfce4bf7d3c9bd7c829b963192f125e1
AmedDavid <amukoahdavid@gmail.com>
2025-11-23 23:43:01 +0300

    Add base_case class to server.py for handling file operations and defining case methods

--- server.py
+++ server.py
@@ -2,6 +2,27 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import os
 
+class base_case(object):
+    """Parent for case handlers."""
+
+    def handle_file(self, handler, full_path):
+        try:
+            with open(full_path, 'rb') as reader:
+                content = reader.read()
+            handler.send_content(content)
+        except IOError as msg:
+            msg = "'{0}' cannot be read: {1}".format(full_path, msg)
+            handler.handle_error(msg)
+
+    def index_path(self, handler):
+        return os.path.join(handler.full_path, 'index.html')
+
+    def test(self, handler):
+        assert False, 'Not implemented.'
+
+    def act(self, handler):
+        assert False, 'Not implemented.'
+
 class case_no_file(object):
     '''File or directory does not exist.'''
 
